27487b5579ad96a0d4ace7fd2e93ba1ee32e6773
Arthur Guiot <arguiot@gmail.com>
2017-05-08 17:44:44 +1000

    Updated some files
    gulpfiles.js and package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 var gulp = require('gulp'),
     rename = require('gulp-rename')
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify')
+    babel = require('gulp-babel');
 gulp.task("minify", function () {
 	gulp.src('src/*.js')
+	.pipe(babel({
+		presets: ['env']
+	}))
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('dist'))
 });
 gulp.task("copy", function () {
 	gulp.src('src/*.js')
+	.pipe(babel({
+		presets: ['env']
+	}))
     .pipe(gulp.dest('dist'))
 });
 gulp.task('default', [ 'minify', 'copy' ]);
