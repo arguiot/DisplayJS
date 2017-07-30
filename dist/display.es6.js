8aa83489fb7895731884533aa616658b1cc0e911
Arthur Guiot <arguiot@gmail.com>
2017-07-30 21:55:00 +0200

    Added components and time_rel

--- dist/display.es6.js
+++ dist/display.es6.js
@@ -280,7 +280,7 @@ class DisplayJS {
 	}
 	select(str) {
 		if (this.isElement(str)) {
-			return str;
+			return new Array(str);
 		}
 		return document.querySelectorAll(str);
 	}
@@ -453,6 +453,69 @@ class DisplayJS {
 				el && typeof el === "object" && el !== null && el.nodeType === 1 && typeof el.nodeName==="string"
 		);
 	}
+	component (name, callback) {
+		class component extends HTMLElement {
+			constructor () {
+				super();
+				callback(this);
+			}
+		}
+		customElements.define(name, component);
+	}
+	time_rel(time) {
+		switch (typeof time) {
+		case "number":
+			break;
+		case "string":
+			time = +new Date(time);
+			break;
+		case "object":
+			if (time.constructor === Date) time = time.getTime();
+			break;
+		default:
+			time = +new Date();
+		}
+		const time_formats = [
+			[60, "seconds", 1], // 60
+			[120, "1 minute ago", "1 minute from now"], // 60*2
+			[3600, "minutes", 60], // 60*60, 60
+			[7200, "1 hour ago", "1 hour from now"], // 60*60*2
+			[86400, "hours", 3600], // 60*60*24, 60*60
+			[172800, "Yesterday", "Tomorrow"], // 60*60*24*2
+			[604800, "days", 86400], // 60*60*24*7, 60*60*24
+			[1209600, "Last week", "Next week"], // 60*60*24*7*4*2
+			[2419200, "weeks", 604800], // 60*60*24*7*4, 60*60*24*7
+			[4838400, "Last month", "Next month"], // 60*60*24*7*4*2
+			[29030400, "months", 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
+			[58060800, "Last year", "Next year"], // 60*60*24*7*4*12*2
+			[2903040000, "years", 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
+			[5806080000, "Last century", "Next century"], // 60*60*24*7*4*12*100*2
+			[58060800000, "centuries", 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
+		];
+		let seconds = (+new Date() - time) / 1000;
+		let token = "ago";
+		let list_choice = 1;
+
+		if (seconds == 0) {
+			return "Just now";
+		}
+		if (seconds < 0) {
+			seconds = Math.abs(seconds);
+			token = "from now";
+			list_choice = 2;
+		}
+		let i = 0;
+		let format;
+		while (format == time_formats[i++]) {
+			if (seconds < format[0]) {
+				if (typeof format[2] == "string")
+					return format[list_choice];
+				else
+					return `${Math.floor(seconds / format[2])} ${format[1]} ${token}`;
+			}
+		}
+		return time;
+	}
 	// Math and array manipulation
 	arange(start, end, step, offset) {
 		const len = (Math.abs(end - start) + ((offset || 0) * 2)) / (step || 1) + 1;
@@ -595,7 +658,7 @@ class DisplayJS {
 				return this.shiftComma(this.countResult(intArr,2),commaSum);
 			},
 			isSafeInteger(result) {
-				if(result<=-(Math.pow(2, 53)-1)||result>=(Math.pow(2, 53)-1)) throw "The result is not a safe integer.";
+				if(result<=-(Math.pow(2, 53)-1)||result>=(Math.pow(2, 53)-1)) throw "The result is not a safe integer."; 
 			},
 			shiftComma(result, commaPos) {
 				return this.toExponent(this.countDecimals([result]),-commaPos)[0];
@@ -664,7 +727,7 @@ class DisplayJS {
 }
 // Retro compatibility
 class _DOM_DJS extends DisplayJS {}
-// Browaserify / Node.js
+// Browserify / Node.js
 if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports) {
 	module.exports = new DisplayJS;
 }
--- dist/display.js
+++ dist/display.js
@@ -358,7 +358,7 @@ var DisplayJS = function () {
 		key: "select",
 		value: function select(str) {
 			if (this.isElement(str)) {
-				return str;
+				return new Array(str);
 			}
 			return document.querySelectorAll(str);
 		}
@@ -586,6 +586,78 @@ var DisplayJS = function () {
 			return (typeof HTMLElement === "undefined" ? "undefined" : _typeof(HTMLElement)) === "object" ? el instanceof HTMLElement : //DOM2
 			el && (typeof el === "undefined" ? "undefined" : _typeof(el)) === "object" && el !== null && el.nodeType === 1 && typeof el.nodeName === "string";
 		}
+	}, {
+		key: "component",
+		value: function component(name, callback) {
+			var component = function (_HTMLElement) {
+				_inherits(component, _HTMLElement);
+
+				function component() {
+					_classCallCheck(this, component);
+
+					var _this5 = _possibleConstructorReturn(this, (component.__proto__ || Object.getPrototypeOf(component)).call(this));
+
+					callback(_this5);
+					return _this5;
+				}
+
+				return component;
+			}(HTMLElement);
+
+			customElements.define(name, component);
+		}
+	}, {
+		key: "time_rel",
+		value: function time_rel(time) {
+			switch (typeof time === "undefined" ? "undefined" : _typeof(time)) {
+				case "number":
+					break;
+				case "string":
+					time = +new Date(time);
+					break;
+				case "object":
+					if (time.constructor === Date) time = time.getTime();
+					break;
+				default:
+					time = +new Date();
+			}
+			var time_formats = [[60, "seconds", 1], // 60
+			[120, "1 minute ago", "1 minute from now"], // 60*2
+			[3600, "minutes", 60], // 60*60, 60
+			[7200, "1 hour ago", "1 hour from now"], // 60*60*2
+			[86400, "hours", 3600], // 60*60*24, 60*60
+			[172800, "Yesterday", "Tomorrow"], // 60*60*24*2
+			[604800, "days", 86400], // 60*60*24*7, 60*60*24
+			[1209600, "Last week", "Next week"], // 60*60*24*7*4*2
+			[2419200, "weeks", 604800], // 60*60*24*7*4, 60*60*24*7
+			[4838400, "Last month", "Next month"], // 60*60*24*7*4*2
+			[29030400, "months", 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
+			[58060800, "Last year", "Next year"], // 60*60*24*7*4*12*2
+			[2903040000, "years", 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
+			[5806080000, "Last century", "Next century"], // 60*60*24*7*4*12*100*2
+			[58060800000, "centuries", 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
+			];
+			var seconds = (+new Date() - time) / 1000;
+			var token = "ago";
+			var list_choice = 1;
+
+			if (seconds == 0) {
+				return "Just now";
+			}
+			if (seconds < 0) {
+				seconds = Math.abs(seconds);
+				token = "from now";
+				list_choice = 2;
+			}
+			var i = 0;
+			var format = void 0;
+			while (format == time_formats[i++]) {
+				if (seconds < format[0]) {
+					if (typeof format[2] == "string") return format[list_choice];else return Math.floor(seconds / format[2]) + " " + format[1] + " " + token;
+				}
+			}
+			return time;
+		}
 		// Math and array manipulation
 
 	}, {
@@ -624,19 +696,19 @@ var DisplayJS = function () {
 	}, {
 		key: "sum",
 		value: function sum(array) {
-			var _this5 = this;
+			var _this6 = this;
 
 			return array.reduce(function (a, b) {
-				return _this5.math().add(a, b);
+				return _this6.math().add(a, b);
 			}, 0);
 		}
 	}, {
 		key: "multiply",
 		value: function multiply(array) {
-			var _this6 = this;
+			var _this7 = this;
 
 			return array.reduce(function (a, b) {
-				return _this6.math().mul(a, b);
+				return _this7.math().mul(a, b);
 			}, 0);
 		}
 	}, {
@@ -683,10 +755,10 @@ var DisplayJS = function () {
 	}, {
 		key: "median",
 		value: function median(array) {
-			var _this7 = this;
+			var _this8 = this;
 
 			array.sort(function (a, b) {
-				return _this7.math().sub(a, b);
+				return _this8.math().sub(a, b);
 			});
 			var half = Math.floor(this.math().div(array.length, 2));
 			if (array.length % 2) {
@@ -858,7 +930,7 @@ var _DOM_DJS = function (_DisplayJS) {
 
 	return _DOM_DJS;
 }(DisplayJS);
-// Browaserify / Node.js
+// Browserify / Node.js
 
 
 if (typeof module !== "undefined" && typeof exports !== "undefined" && module.exports) {
