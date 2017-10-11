2fada268ef12fec83bd233da09eff23a61e6e6e6
Arthur Guiot <arguiot@gmail.com>
2017-10-11 20:01:09 +0800

    Now using eye.js for tests

---
+++ __test__/test.js
@@ -0,0 +1,44 @@
+const djs = require(path.join(testFileDir, "../dist/display.es6"));
+eye.describe("Math part", () => {
+	eye.test("$.extend()", "node",
+		$ => $(djs.extend({"a": 1, "b": 3}, {"a": 2, "c": 6})).Equal({"a": 2, "b": 3, "c": 6})
+	)
+	eye.test("$.linespace()", "node",
+		$ => $(djs.linespace(0, 20, 10)).Equal([ 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 ])
+	)
+	eye.test("$.reshape()", "node",
+		$ => $(djs.reshape(djs.range(5), 2)).Equal([ [ 0, 1 ], [ 2, 3 ], [ 4, 5 ] ])
+	)
+	eye.test("$.flatten()", "node",
+		$ => $(djs.flatten([ [ 0, 1 ], [ 2, 3 ], [ 4, 5 ] ])).Equal(djs.range(5))
+	)
+	eye.test("$.drop()", "node",
+		$ => $(djs.drop(djs.range(5), -1)).Equal(djs.range(4))
+	)
+	eye.test("$.rmFromArray()", "node",
+		$ => $(djs.rmFromArray(djs.range(10), a => a % 2 == 0)).Equal([ 1, 3, 5, 7, 9 ])
+	)
+	var aDay = 24 * 60 * 60 * 1000;
+	eye.test("$.time_ago()", "node",
+		$ => $(djs.time_ago(new Date(Date.now() - aDay))).Match(/Yesterday/)
+	)
+	eye.test("$.sum()", "node",
+		$ => $(djs.sum(djs.range(100))).Equal(5050)
+	)
+	eye.test("$.multiply()", "node",
+		$ => $(djs.multiply(djs.arange(1, 5, 1))).Equal(120)
+	)
+	eye.test("$.average()", "node",
+		$ => $(djs.average([53, 9, 8, 37, 4])).Equal(22.2)
+	)
+	eye.test("$.median()", "node",
+		$ => $(djs.median([53, 9, 8, 37, 4])).Equal(9)
+	)
+	eye.test("$.predict()", "node",
+		$ => $(djs.predict([[1,3],[2,5],[3,7]], 4)).Equal(9)
+	)
+	eye.test("$.math", "node",
+		$ => $(djs.math.mul(djs.math.div(1, 3), 6)).Equal(2),
+		$ => $(djs.math.add(0.2, 0.4)).Equal(0.6)
+	)
+})
--- dist/display.es6.js
+++ dist/display.es6.js
@@ -181,9 +181,9 @@ class DisplayJS {
 		}
 	}
 	toNodeList(el){
-		el.setAttribute('wrapNodeList','');
-		const list = document.querySelectorAll('[wrapNodeList]');
-		el.removeAttribute('wrapNodeList');
+		el.setAttribute("wrapNodeList","");
+		const list = document.querySelectorAll("[wrapNodeList]");
+		el.removeAttribute("wrapNodeList");
 		return list;
 	}
 	// custom repeat function
@@ -291,7 +291,7 @@ class DisplayJS {
 		window.scroll(x, y);
 	}
 	scrollTop (el) {
-		el = el != null ? el : document.body.scrollTop == 0 ? this.toNodeList(document.documentElement) : this.toNodeList(document.body)
+		el = el != null ? el : document.body.scrollTop == 0 ? this.toNodeList(document.documentElement) : this.toNodeList(document.body);
 		el = this.s(el);
 		return el[0].scrollTop;
 	}
@@ -339,7 +339,7 @@ class DisplayJS {
 	}
 	css(el, name, value) {
 		el = this.s(el);
-		if (typeof(name) == 'object') {
+		if (typeof(name) == "object") {
 			for (let i in name) {
 				el[0].style[i] = name[i];
 			}
@@ -479,10 +479,10 @@ class DisplayJS {
 		return array.reduce((a, b) => a.concat(b), []);
 	}
 	drop(array, val) {
-		return val > 0 ? array.slice(val, array.length) : array.slice(0, array.length + val)
+		return val > 0 ? array.slice(val, array.length) : array.slice(0, array.length + val);
 	}
 	isIn(array, val) {
-		return array.includes(val) ? true : false
+		return array.includes(val) ? true : false;
 	}
 	rmFromArray(array, condition) {
 		const obj = [];
@@ -498,7 +498,7 @@ class DisplayJS {
 		el = this.s(el);
 		this.ajax(url, "GET", "", text => {
 			try {
-				el[0].innerHTML = text
+				el[0].innerHTML = text;
 				callback();
 			} catch(e) {
 				callback(e);
@@ -526,8 +526,8 @@ class DisplayJS {
 	}
 	get(url, callback, parse=false) {
 		this.ajax(url, "GET", "", (data) => {
-			parse ? callback(JSON.parse(data)) : callback(data)
-		})
+			parse ? callback(JSON.parse(data)) : callback(data);
+		});
 	}
 	// create your own $.var() like function
 	custom(targetAttr, callback, push) {
@@ -813,7 +813,7 @@ class DisplayJS {
 	}
 	then(toCall, callback) {
 		try {
-			callback(toCall())
+			callback(toCall());
 		} catch (e) {
 			throw "DisplayJS: " + e;
 		}
@@ -843,11 +843,11 @@ if (typeof define === "function" && define.amd) {
 	define(() => new DisplayJS);
 // CommonJS and Node.js module support.
 } else if (typeof exports !== "undefined") {
-		// Support Node.js specific `module.exports` (which can be a function)
+	// Support Node.js specific `module.exports` (which can be a function)
 	if (typeof module !== "undefined" && module.exports) {
 		exports = module.exports = new DisplayJS;
 	}
-		// But always support CommonJS module 1.1.1 spec (`exports` cannot be a function)
+	// But always support CommonJS module 1.1.1 spec (`exports` cannot be a function)
 	exports.DisplayJS = new DisplayJS;
 } else if (typeof global !== "undefined") {
 	global.DisplayJS = new DisplayJS;
